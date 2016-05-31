4d2d783531479ec5ba6bbea6caea0d5781573901
Badassjoker <flute5213@gmail.com>
2016-05-31 22:35:04 +0800

    Update TPSN_node2.c

--- TPSN_node2.c
+++ TPSN_node2.c
@@ -1,61 +1,3 @@
-/**************************************************************************************************
-  Filename:       SampleApp.c
-  Revised:        $Date: 2009-03-18 15:56:27 -0700 (Wed, 18 Mar 2009) $
-  Revision:       $Revision: 19453 $
-
-  Description:    Sample Application (no Profile).
-
-
-  Copyright 2007 Texas Instruments Incorporated. All rights reserved.
-
-  IMPORTANT: Your use of this Software is limited to those specific rights
-  granted under the terms of a software license agreement between the user
-  who downloaded the software, his/her employer (which must be your employer)
-  and Texas Instruments Incorporated (the "License").  You may not use this
-  Software unless you agree to abide by the terms of the License. The License
-  limits your use, and you acknowledge, that the Software may not be modified,
-  copied or distributed unless embedded on a Texas Instruments microcontroller
-  or used solely and exclusively in conjunction with a Texas Instruments radio
-  frequency transceiver, which is integrated into your product.  Other than for
-  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
-  works of, modify, distribute, perform, display or sell this Software and/or
-  its documentation for any purpose.
-
-  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
-  PROVIDED ìAS IS?WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
-  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
-  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
-  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
-  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
-  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
-  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
-  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
-
-  Should you have any questions regarding your right to use this Software,
-  contact Texas Instruments Incorporated at www.TI.com.
-**************************************************************************************************/
-
-/*********************************************************************
-  This application isn't intended to do anything useful, it is
-  intended to be a simple example of an application's structure.
-
-  This application sends it's messages either as broadcast or
-  broadcast filtered group messages.  The other (more normal)
-  message addressing is unicast.  Most of the other sample
-  applications are written to support the unicast message model.
-
-  Key control:
-    SW1:  Sends a flash command to all devices in Group 1.
-    SW2:  Adds/Removes (toggles) this device in and out
-          of Group 1.  This will enable and disable the
-          reception of the flash command.
-*********************************************************************/
-
-/*********************************************************************
- * INCLUDES
- */
 #include <math.h>
 #include "OSAL.h"
 #include "ZGlobals.h"
@@ -63,34 +5,13 @@
 #include "aps_groups.h"
 #include "ZDApp.h"
 #include "OSAL_Clock.h"
-
 #include "SampleApp.h"
 #include "SampleAppHw.h"
-
 #include "OnBoard.h"
-
-/* HAL */
 #include "hal_lcd.h"
 #include "hal_led.h"
 #include "hal_key.h"
-
-#include  "MT_UART.h" //¥À¥¶”√”⁄¥Æø⁄
-
-/*********************************************************************
- * MACROS
- */
-
-/*********************************************************************
- * CONSTANTS
- */
-
-/*********************************************************************
- * TYPEDEFS
- */
-
-/*********************************************************************
- * GLOBAL VARIABLES
- */
+#include  "MT_UART.h"
 
 // This list should be filled with Application specific Cluster IDs.
 const cId_t SampleApp_ClusterList[SAMPLEAPP_MAX_CLUSTERS] =
@@ -118,17 +39,6 @@ const SimpleDescriptionFormat_t SampleApp_SimpleDesc =
 // way it's defined in this sample app it is define in RAM.
 endPointDesc_t SampleApp_epDesc;
 
-/*********************************************************************
- * EXTERNAL VARIABLES
- */
-
-/*********************************************************************
- * EXTERNAL FUNCTIONS
- */
-
-/*********************************************************************
- * LOCAL VARIABLES
- */
 uint8 SampleApp_TaskID;   // Task ID for internal task/event processing
                           // This variable will be received when
                           // SampleApp_Init() is called.
@@ -144,9 +54,6 @@ aps_Group_t SampleApp_Group;
 uint8 SampleAppPeriodicCounter = 0;
 uint8 SampleAppFlashCounter = 0;
 
-/*********************************************************************
- * LOCAL FUNCTIONS
- */
 void SampleApp_HandleKeys( uint8 shift, uint8 keys );
 void SampleApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );
 void SampleApp_SendPeriodicMessage( void );
@@ -157,28 +64,6 @@ void SampleApp_ConvertToSETTIME(uint32 seconds);
 void SampleApp_SendClockMessage( void );
 uint32 SampleApp_ConvertToT2T3(void);
 
-/*********************************************************************
- * NETWORK LAYER CALLBACKS
- */
-
-/*********************************************************************
- * PUBLIC FUNCTIONS
- */
-
-/*********************************************************************
- * @fn      SampleApp_Init
- *
- * @brief   Initialization function for the Generic App Task.
- *          This is called during initialization and should contain
- *          any application specific initialization (ie. hardware
- *          initialization/setup, table initialization, power up
- *          notificaiton ... ).
- *
- * @param   task_id - the ID assigned by OSAL.  This ID should be
- *                    used to send messages and set timers.
- *
- * @return  none
- */
 void SampleApp_Init( uint8 task_id )
 {
   SampleApp_TaskID = task_id;
@@ -187,9 +72,9 @@ void SampleApp_Init( uint8 task_id )
   
   osal_setClock(0);
   
-  MT_UartInit();//¥Æø⁄≥ı ºªØ
-  MT_UartRegisterTaskID(task_id);//µ«º«»ŒŒÒ∫≈
-  //HalUARTWrite(0,"Hello World\n",12); //£®¥Æø⁄0£¨'◊÷∑˚'£¨◊÷∑˚∏ˆ ˝°££©
+  MT_UartInit();//‰∏≤Âè£ÂàùÂßãÂåñ
+  MT_UartRegisterTaskID(task_id);//ÁôªËÆ∞‰ªªÂä°Âè∑
+  //HalUARTWrite(0,"Hello World\n",12); //Ôºà‰∏≤Âè£0Ôºå'Â≠óÁ¨¶'ÔºåÂ≠óÁ¨¶‰∏™Êï∞„ÄÇÔºâ
   
   // Device hardware initialization can be added here or in main() (Zmain.c).
   // If the hardware is application specific - add it here.
@@ -247,19 +132,6 @@ void SampleApp_Init( uint8 task_id )
 #endif
 }
 
-/*********************************************************************
- * @fn      SampleApp_ProcessEvent
- *
- * @brief   Generic Application Task event processor.  This function
- *          is called to process all events for the task.  Events
- *          include timers, messages and any other user defined events.
- *
- * @param   task_id  - The OSAL assigned task ID.
- * @param   events - events to process.  This is a bit map and can
- *                   contain more than one event.
- *
- * @return  none
- */
 uint16 SampleApp_ProcessEvent( uint8 task_id, uint16 events )
 {
   afIncomingMSGPacket_t *MSGpkt;
@@ -334,21 +206,6 @@ uint16 SampleApp_ProcessEvent( uint8 task_id, uint16 events )
   return 0;
 }
 
-/*********************************************************************
- * Event Generation Functions
- */
-/*********************************************************************
- * @fn      SampleApp_HandleKeys
- *
- * @brief   Handles all key events for this device.
- *
- * @param   shift - true if in shift/alt.
- * @param   keys - bit field for key events. Valid entries:
- *                 HAL_KEY_SW_2
- *                 HAL_KEY_SW_1
- *
- * @return  none
- */
 void SampleApp_HandleKeys( uint8 shift, uint8 keys )
 {
   (void)shift;  // Intentionally unreferenced parameter
@@ -383,22 +240,6 @@ void SampleApp_HandleKeys( uint8 shift, uint8 keys )
     }
   }
 }
-
-/*********************************************************************
- * LOCAL FUNCTIONS
- */
-
-/*********************************************************************
- * @fn      SampleApp_MessageMSGCB
- *
- * @brief   Data message processor callback.  This function processes
- *          any incoming data - probably from other devices.  So, based
- *          on cluster ID, perform the intended action.
- *
- * @param   none
- *
- * @return  none
- */
 
 unsigned char T1[10];
 unsigned char T2T3[10];
@@ -452,21 +293,12 @@ void SampleApp_MessageMSGCB( afIncomingMSGPacket_t *pkt )
   }
 }
 
-/*********************************************************************
- * @fn      SampleApp_SendPeriodicMessage
- *
- * @brief   Send the periodic message.
- *
- * @param   none
- *
- * @return  none
- */
 void SampleApp_SendPeriodicMessage( void )
 {
   if ( AF_DataRequest( &SampleApp_Periodic_DstAddr, &SampleApp_epDesc,
                        SAMPLEAPP_PERIODIC_CLUSTERID,
-                       10,//◊÷Ω⁄ ˝
-                       T1,//÷∏’ÎÕ∑
+                       10,//Â≠óËäÇÊï∞
+                       T1,//ÊåáÈíàÂ§¥
                        &SampleApp_TransID,
                        AF_DISCV_ROUTE,
                        AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
@@ -478,15 +310,6 @@ void SampleApp_SendPeriodicMessage( void )
   }
 }
 
-/*********************************************************************
- * @fn      SampleApp_SendFlashMessage
- *
- * @brief   Send the flash message to group 1.
- *
- * @param   flashTime - in milliseconds
- *
- * @return  none
- */
 void SampleApp_SendFlashMessage( uint16 flashTime )
 {
   uint8 buffer[3];
@@ -509,8 +332,6 @@ void SampleApp_SendFlashMessage( uint16 flashTime )
   }
 }
 
-/*********************************************************************
-*********************************************************************/
 void SampleApp_ConvertToT1(uint32 seconds)
 {
   uint8 tempUsing = 0;
@@ -611,8 +432,8 @@ void SampleApp_SendClockMessage( void )
   if ( AF_DataRequest( &SampleApp_Flash_DstAddr,
                        &SampleApp_epDesc,
                        SAMPLEAPP_FLASH_CLUSTERID,
-                       10,//◊÷Ω⁄ ˝
-                       T1,//÷∏’ÎÕ∑
+                       10,//Â≠óËäÇÊï∞
+                       T1,//ÊåáÈíàÂ§¥
                        &SampleApp_TransID,
                        AF_DISCV_ROUTE,
                        AF_DEFAULT_RADIUS ) == afStatus_SUCCESS )
